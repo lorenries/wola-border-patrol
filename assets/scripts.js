a5597b187c1dbe25d728d292cdd6c89951b6d2cb
lorenries <lorenries@users.noreply.github.com>
2017-09-06 17:40:18 -0400

    almost done

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,2 +1,50 @@
-var disabledLink = document.querySelector('.disabled');
-disabledLink.addEventListener('click', function(e) { e.preventDefault(); })
+document.addEventListener('DOMContentLoaded', function(e) {
+
+	feather.replace( {"height":"1em", "width": "1em"} );
+
+	var disabledLink = document.querySelector('.disabled');
+	
+	if (disabledLink) {
+		disabledLink.addEventListener('click', function(e) { e.preventDefault(); });
+	}
+
+	var popup = document.querySelector('.js-popup');
+
+	var popupClose = document.querySelector('.js-close-popup');
+
+	function scrollFn() {
+
+		if(window.pageYOffset > 500) {
+
+			popup.classList.remove('dn');
+
+			return;
+
+		}
+	}
+
+
+	if (popup) {
+		document.addEventListener('scroll', scrollFn, false);
+	}
+
+	if (popupClose) {
+
+		popupClose.addEventListener('click', function(e) {
+
+			e.preventDefault();
+			popup.classList.add('dn');
+
+			// kill scroll listener
+			document.removeEventListener('scroll', scrollFn, false);
+
+
+		}, false);
+	}
+
+
+
+}, false);
+
+
+
