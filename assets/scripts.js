84b0e4b6bef4191c2bb041f248283bf64bd0adf5
lorenries <lorenries@users.noreply.github.com>
2017-09-13 17:44:16 -0400

    final touches

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,50 +1,64 @@
 document.addEventListener('DOMContentLoaded', function(e) {
 
-	feather.replace( {"height":"1em", "width": "1em"} );
+    feather.replace({ "height": "1em", "width": "1em" });
 
-	var disabledLink = document.querySelector('.disabled');
-	
-	if (disabledLink) {
-		disabledLink.addEventListener('click', function(e) { e.preventDefault(); });
-	}
+    // Initialize polyfill for 'position: sticky'
 
-	var popup = document.querySelector('.js-popup');
+    var sticky = document.querySelector('.sticky');
 
-	var popupClose = document.querySelector('.js-close-popup');
+    if (sticky) {
 
-	function scrollFn() {
+        new StickyState(sticky);
 
-		if(window.pageYOffset > 500) {
+    }
 
-			popup.classList.remove('dn');
+    var disabledLink = document.querySelector('.disabled');
 
-			return;
+    if (disabledLink) {
+        disabledLink.addEventListener('click', function(e) { e.preventDefault(); });
+    }
 
-		}
-	}
+    // Popup Function
+
+    function popup() {
+
+        var popup = document.querySelector('.js-popup');
+        var popupClose = document.querySelector('.js-close-popup');
+        var footerHeight = document.querySelector('footer').offsetHeight;
+
+        function scrollFn() {
+
+            if (window.pageYOffset > 500 < ((document.body.clientHeight - footerHeight) - (window.pageYOffset + window.innerHeight))) {
+
+                popup.classList.remove('dn');
+
+            } else if ((window.pageYOffset + window.innerHeight) >= (document.body.clientHeight - footerHeight)) {
+
+                popup.classList.add('dn');
+
+            }
+        }
+
+        if (popup) {
+            document.addEventListener('scroll', scrollFn, false);
+        }
+
+        if (popupClose) {
+
+            popupClose.addEventListener('click', function(e) {
+
+                e.preventDefault();
+                popup.classList.add('dn');
+
+                // kill scroll listener
+                document.removeEventListener('scroll', scrollFn, false);
 
 
-	if (popup) {
-		document.addEventListener('scroll', scrollFn, false);
-	}
+            }, false);
+        }
 
-	if (popupClose) {
+    }
 
-		popupClose.addEventListener('click', function(e) {
-
-			e.preventDefault();
-			popup.classList.add('dn');
-
-			// kill scroll listener
-			document.removeEventListener('scroll', scrollFn, false);
-
-
-		}, false);
-	}
-
-
+    popup();
 
 }, false);
-
-
-
